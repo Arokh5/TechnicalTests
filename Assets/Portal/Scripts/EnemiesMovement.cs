6cb0ed5aaa493dd6779bb985c15874bbcddd2cc8
Dani Reyes <dani-kingdom@hotmail.com>
2018-02-02 18:19:44 +0100

    Add portal that teleports Enemies

---
+++ Assets/Portal/Scripts/EnemiesMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemiesMovement : MonoBehaviour {
+
+    public GameObject portalToSpawn;
+    public bool teleported;
+
+	// Use this for initialization
+	void Start () {
+        portalToSpawn = GameObject.Find("PlaneToSpawn");
+        teleported = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Translate(this.transform.forward * Time.deltaTime * 2.0f,Space.World);
+	}
+
+    private void OnTriggerEnter(Collider other) {
+        if (!teleported) {
+            if (other.transform.tag == "Portal") {
+                teleported = true;
+                Vector3 newPos = portalToSpawn.transform.position - other.transform.position + this.transform.position;
+                this.transform.position = newPos;
+            }
+        }
+    }
+}
---
+++ Assets/Portal/Scripts/PortalCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalCamera : MonoBehaviour {
+
+    public Transform playerCamera, portal, otherPortal;
+	
+	void Update () {
+        Vector3 playerOffsetFromPortal = playerCamera.transform.position - otherPortal.transform.position;
+        this.transform.position = portal.transform.position + playerOffsetFromPortal;
+
+        float angularDifference = Quaternion.Angle(portal.rotation, otherPortal.rotation);
+
+        Quaternion portalRotationDifference = Quaternion.AngleAxis(angularDifference, Vector3.up);
+        Vector3 newCameraDirection = portalRotationDifference * playerCamera.forward;
+        transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
+
+
+	}
+}
