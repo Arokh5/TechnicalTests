4292851385655d755cf56f2b5520d15368b9b029
Orphen <sergiramirezgarcia@gmail.com>
2018-02-03 21:41:16 +0100

    Frontal camera do not occlude with walls

---
+++ Assets/Scripts/FrontCameraController.cs
@@ -0,0 +1,215 @@
+﻿using UnityEngine;
+
+// This class corresponds to the 3rd person camera features.
+public class FrontCameraController : MonoBehaviour
+{
+    public Transform player;                                           // Player's reference.
+    public Vector3 pivotOffset = new Vector3(0.0f, 2.0f, 0.0f);        // Offset to repoint the camera.
+    public Vector3 camOffset = new Vector3(0f, 0f, -7.0f);             // Offset to relocate the camera related to the player position.
+    public float smooth = 10f;                                         // Speed of camera responsiveness.
+    public float horizontalAimingSpeed = 6f;                           // Horizontal turn speed.
+    public float verticalAimingSpeed = 6f;                             // Vertical turn speed.
+    public float maxVerticalAngle = 30f;                               // Camera max clamp angle. 
+    public float minVerticalAngle = -60f;                              // Camera min clamp angle.
+    public string XAxis = "Mouse X";                                   // The default horizontal axis input name.
+    public string YAxis = "Mouse Y";                                   // The default vertical axis input name.
+
+    private float angleH = 0;                                          // Float to store camera horizontal angle related to mouse movement.
+    private float angleV = 0;                                          // Float to store camera vertical angle related to mouse movement.
+    private Transform cam;                                             // This transform.
+    private Vector3 relCameraPos;                                      // Current camera position relative to the player.
+    private float relCameraPosMag;                                     // Current camera distance to the player.
+    private Vector3 smoothPivotOffset;                                 // Camera current pivot offset on interpolation.
+    private Vector3 smoothCamOffset;                                   // Camera current offset on interpolation.
+    private Vector3 targetPivotOffset;                                 // Camera pivot offset target to iterpolate.
+    private Vector3 targetCamOffset;                                   // Camera offset target to interpolate.
+    private float defaultFOV;                                          // Default camera Field of View.
+    private float targetFOV;                                           // Target camera Field of View.
+    private float targetMaxVerticalAngle;                              // Custom camera max vertical clamp angle.
+    private const float lerpSpeed = 0.5f;
+    private const int zoomRate = 50;
+    private const int environmentLayer = 8;
+
+    // Get the camera horizontal angle.
+    public float GetH { get { return angleH; } }
+
+    void Awake()
+    {
+        // Reference to the camera transform.
+        cam = transform;
+
+        // Set camera default position.
+        cam.position = player.position + Quaternion.identity * pivotOffset + Quaternion.identity * camOffset;
+        cam.rotation = Quaternion.Euler(0f, 180f, 0f);
+
+        // Get camera position relative to the player, used for collision test.
+        relCameraPos = transform.position - player.position;
+        relCameraPosMag = relCameraPos.magnitude - 0.5f;
+
+        // Set up references and default values.
+        smoothPivotOffset = pivotOffset;
+        smoothCamOffset = camOffset;
+        defaultFOV = cam.GetComponent<Camera>().fieldOfView;
+        angleH = player.eulerAngles.y;
+
+        ResetTargetOffsets();
+        ResetFOV();
+        ResetMaxVerticalAngle();
+    }
+
+    void Update()
+    {
+        angleH = player.eulerAngles.y;
+
+        // Set vertical movement limit.
+        angleV = Mathf.Clamp(angleV, minVerticalAngle, targetMaxVerticalAngle);
+
+        // Set camera orientation.
+        Quaternion camYRotation = Quaternion.Euler(0, angleH, 0);
+        Quaternion aimRotation = Quaternion.Euler(-angleV, angleH + 180f, 0);
+        cam.rotation = aimRotation;
+
+        // Set FOV.
+        cam.GetComponent<Camera>().fieldOfView = Mathf.Lerp(cam.GetComponent<Camera>().fieldOfView, targetFOV, Time.deltaTime);
+
+        //camOffset.z += Input.GetAxis("Mouse ScrollWheel") * Time.deltaTime * zoomRate * Mathf.Abs(camOffset.z);
+        SetCamOffset(ref camOffset);
+
+        // Test for collision with the environment based on current camera position.
+        Vector3 baseTempPosition = player.position + camYRotation * targetPivotOffset;
+        Vector3 noCollisionOffset = targetCamOffset;
+        for (float zOffset = targetCamOffset.z; zOffset <= 0; zOffset += 0.5f)
+        {
+            noCollisionOffset.z = zOffset;
+
+            if (DoubleViewingPosCheck(baseTempPosition + aimRotation * noCollisionOffset, Mathf.Abs(zOffset)) || zOffset == 0)
+            {
+                break;
+            }
+        }
+
+        // Repostition the camera.
+        smoothPivotOffset = Vector3.Lerp(smoothPivotOffset, targetPivotOffset, smooth * Time.deltaTime);
+        smoothCamOffset = Vector3.Lerp(smoothCamOffset, noCollisionOffset, smooth * Time.deltaTime);
+
+        cam.position = player.position + camYRotation * smoothPivotOffset + aimRotation * smoothCamOffset;
+    }
+
+    // Set camera offsets to custom values.
+    public void SetTargetOffsets(Vector3 newPivotOffset, Vector3 newCamOffset)
+    {
+        targetPivotOffset = newPivotOffset;
+        targetCamOffset = newCamOffset;
+    }
+
+    // Reset camera offsets to default values.
+    public void ResetTargetOffsets()
+    {
+        targetPivotOffset = pivotOffset;
+        targetCamOffset = camOffset;
+    }
+
+    // Reset the camera vertical offset.
+    public void ResetYCamOffset()
+    {
+        targetCamOffset.y = camOffset.y;
+    }
+
+    // Set camera vertical offset.
+    public void SetYCamOffset(float y)
+    {
+        targetCamOffset.y = y;
+    }
+
+    // Set camera horizontal offset.
+    public void SetXCamOffset(float x)
+    {
+        targetCamOffset.x = x;
+    }
+
+    // Set camera offset.
+    public void SetCamOffset(ref Vector3 camOffset)
+    {
+        if (camOffset.z < -15f)
+        {
+            camOffset.z = -15f;
+        }
+        if (camOffset.z > -2f)
+        {
+            camOffset.z = -2f;
+        }
+
+        targetCamOffset = camOffset;
+    }
+
+    // Set custom Field of View.
+    public void SetFOV(float customFOV)
+    {
+        this.targetFOV = customFOV;
+    }
+
+    // Reset Field of View to default value.
+    public void ResetFOV()
+    {
+        this.targetFOV = defaultFOV;
+    }
+
+    // Set max vertical camera rotation angle.
+    public void SetMaxVerticalAngle(float angle)
+    {
+        this.targetMaxVerticalAngle = angle;
+    }
+
+    // Reset max vertical camera rotation angle to default value.
+    public void ResetMaxVerticalAngle()
+    {
+        this.targetMaxVerticalAngle = maxVerticalAngle;
+    }
+
+    // Double check for collisions: concave objects doesn't detect hit from outside, so cast in both directions.
+    bool DoubleViewingPosCheck(Vector3 checkPos, float offset)
+    {
+        float playerFocusHeight = player.GetComponent<CapsuleCollider>().height * 0.5f;
+        return ViewingPosCheck(checkPos, playerFocusHeight) && ReverseViewingPosCheck(checkPos, playerFocusHeight, offset);
+    }
+
+    // Check for collision from camera to player.
+    bool ViewingPosCheck(Vector3 checkPos, float deltaPlayerHeight)
+    {
+        RaycastHit hit;
+
+        // If a raycast from the check position to the player hits something...
+        if (Physics.Raycast(checkPos, player.position + (Vector3.up * deltaPlayerHeight) - checkPos, out hit, relCameraPosMag))
+        {
+            // ... if it is not the player...
+            if (hit.transform.gameObject.layer == environmentLayer)
+            {
+                // This position isn't appropriate.
+                return false;
+            }
+        }
+        // If we haven't hit anything or we've hit the player, this is an appropriate position.
+        return true;
+    }
+
+    // Check for collision from player to camera.
+    bool ReverseViewingPosCheck(Vector3 checkPos, float deltaPlayerHeight, float maxDistance)
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(player.position + (Vector3.up * deltaPlayerHeight), checkPos - player.position, out hit, maxDistance))
+        {
+            if (hit.transform != transform && hit.transform.gameObject.layer == environmentLayer)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Get camera magnitude.
+    public float GetCurrentPivotMagnitude(Vector3 finalPivotOffset)
+    {
+        return Mathf.Abs((finalPivotOffset - smoothPivotOffset).magnitude);
+    }
+}
